dd546417228770b382317c211358e461aba85bc8
Irwing <irwingcorrea95@gmail.com>
2024-06-06 20:18:27 -0300

    alterando titulo no navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
         <nav className="bg-gray-800 p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <Link href="/" className="text-white text-lg font-bold">
-                    Meu Sistema
+                    Manauara Diesel
                 </Link>
                 <div className="flex space-x-4">
                     <Link href="/clientes" className='text-gray-100'>
